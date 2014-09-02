429b3646a97433e49ee3a39d983b385e0329db53
Ryan Clarke <rkclarke@ncsu.edu>
2014-09-02 00:12:09 -0400

    Remove transitional script

--- link.sh
+++ link.sh
@@ -1,36 +1,56 @@
-#!/bin/sh
+#!/bin/bash
+shopt -s nullglob
 
 #save old IFS and update to ignore spaces
 IFS_backup=$IFS
-IFS="\n"
+IFS='
+'
 
-#mod file source and destination directories
-src_dir="minecraft/mods"
-dest_dir="server/mods"
+#parse script arguments
+if [ $# -lt 2 ] || [ $# -gt 3 ] || [ "$1" == "--help" ]; then
+    echo "  Usage: \"link <destination> <source> [blacklist]"
+    exit 0;
+fi
 
-#path back to the root directory
-rel_path="../.."
+destination=$1
+source=$2
+blacklist=$3
 
-#update working directory
-cd "$src_dir"
+echo "\"$destination\" <-- \"$source\" ($blacklist)"
 
-for f in *
-do
-	if [ -e $rel_path/$dest_dir/$f ]; then
-		echo "Skipping: $f"
-	else
-		echo "Linking:  $f"
-		ln -s "$rel_path/$src_dir/$f" "$rel_path/$dest_dir/$f"
-	fi
+#navigate to destination directory
+root_dir=`pwd`
+cd $destination
+
+#clean old links from destination
+echo "Cleaning destination..."
+for i in *; do
+    if [ -h "$i" ]; then
+        rm "$i"
+    fi
 done
 
-cd $rel_path
-while read line
-do
-	echo "Removing (client only): $line"
-	rm -rf "$dest_dir/$line"
-done < clientOnlyMods.dat
+#link files from source to destination
+echo "Generating links..."
+for i in "${source}"/*; do
+    ln -s "$i" `basename "$i"`
+done
+
+#return to local root directory
+cd $root_dir
+
+#remove blacklisted items
+if [ -f "${blacklist}" ]; then
+    echo "Parsing blacklist..."
+    while read i
+    do
+        if [ -e "$destination/$i" ]; then
+            rm "$destination/$i"
+        fi
+    done < $blacklist
+fi
+
+echo "Done!"
 
 #restore old IFS
 IFS=$IFS_backup
-
