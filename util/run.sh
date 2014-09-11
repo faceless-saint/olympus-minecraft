78fbd73ce05b4353d18737e62e5fa1be1ad5cfd9
faceless-saint <faceless.saint@gmail.com>
2014-09-10 23:30:25 -0400

    Update run.sh

--- util/run.sh
+++ util/run.sh
@@ -4,27 +4,54 @@
 ##
 #############################################################
 
-#environment configuration
-SERVER_NAME="olympus"
+#UID validation
+if [ $USER != "root" ] && [ $USER != "minecraft" ]; then
+  echo "Err: script must be run as minecraft or root"
+  exit 1
+fi
 
+#parse script arguments
+case $# in
+    1)
+        SERVER_NAME="$1"
+        BRANCH="master"
+        ;;
+    2)
+        SERVER_NAME="$1"
+        BRANCH="$2"
+        ;;
+    *)
+        echo "Err: invalid arguments"
+        echo "  usage: run.sh <server> [branch]"
+        exit 1
+        ;;
+esac
+
+#insert timestamp
+echo "[`date`]"
+
+#environment configuration
 SERVER_POS="/opt/msm/servers/"$SERVER_NAME""
-MINECRAFT_CMD="/usr/local/bin/msm "$SERVER_NAME""
 GIT_POS=""$SERVER_POS"/git"
+
+#minecraft server manager
+MINECRAFT_CMD="/usr/local/bin/msm "$SERVER_NAME""
 
 #location of blacklist
 BLACKLIST=""$GIT_POS"/util/blacklist.dat"
 
 #stop the server
-$MINECRAFT_CMD stop
+"$MINECRAFT_CMD" stop
 
 #run update scripts
-"$GIT_POS"/util/gitupdate.sh "$GIT_POS"
+"$GIT_POS"/util/gitupdate.sh "$GIT_POS" "$BRANCH"
 "$GIT_POS"/util/linkupdate.sh "$SERVER_POS"/mods "$GIT_POS"/minecraft/mods "$BLACKLIST"
 
 #ensure proper file ownership
 if [ $USER == "root" ]; then
-  chown -R minecraft:minecraft /opt/msm
+    chown -R minecraft:minecraft "$SERVER_POS"
+    chown -R minecraft:minecraft "$GIT_POS"
 fi
 
 #start the server
-$MINECRAFT_CMD start
+"$MINECRAFT_CMD" start
